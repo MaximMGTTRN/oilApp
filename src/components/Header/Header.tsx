ffb93e7a8a0ac7cba09f52fb969f87484e7dbbc5
MGTTRN <mgttrn@mail.ru>
2025-02-18 02:52:46 +0300

    main/Add fixes for header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
-import { Tooltip, Typography } from "@mui/material";
+import { Icon, Tooltip, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import CatalogMenu from "./CatalogMenu/ClatalogMenu";
 import { useLocationContext } from "../../context/LocationContext";
 import "./Header.css";
-
+import MenuIcon from "@mui/icons-material/Menu";
 const Header: React.FC = () => {
   const [scrolled, setScrolled] = useState(false);
   const [catalogOpened, setCatalogOpened] = useState(false);
-  const [isMobile, setIsMobile] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 915);
   const { currentUrl } = useLocationContext();
   const location = useLocation();
 
@@ -111,7 +111,30 @@ const Header: React.FC = () => {
       </header>
     );
   } else {
-    return <></>;
+    return (
+      <header
+        className={`header ${isNeedBackground ? "scrolled" : ""}`}
+        style={{ zIndex: 1000 }}
+      >
+        <div className="header-content">
+          <div className="nav-logo">
+            <Link to="/">
+              <img
+                className="nav-logo-img"
+                src={"/newLogoNoText.png"}
+                alt="Logo"
+              />
+            </Link>
+          </div>
+          <div>
+            <MenuIcon />
+          </div>
+        </div>
+        {catalogOpened && (
+          <CatalogMenu onClose={() => setCatalogOpened(false)} />
+        )}
+      </header>
+    );
   }
 };
 
